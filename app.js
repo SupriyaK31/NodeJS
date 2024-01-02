97589a339d645ccdd6715da6349bc6cf2b5dfd87
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-01-02 23:45:57 +0530

    BodyParser
    BodyParser

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const http=require('http');
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
 
-const route=require('./route');
-
-// const server=http.createServer(route);
-console.log(route.someText);
-const server=http.createServer(route.handler);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+   res.send('<form action="/product" method="post">Title:<input type="text" name="title"/>Size:<input type="text" name="size"/><button type="submit">Add Product</button></form>');
+   console.log('In Middleware');
+});
+app.use('/product',(req,res)=>{
+   console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res)=>{
+   res.send('<h1>Hello from express</h1>');
+    console.log('In another Middleware');
+    
+ });
+const server=http.createServer(app);
 server.listen(3000);
 
